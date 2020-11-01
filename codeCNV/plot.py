f886dcab5db9eb76628b5a1e97ae7d9829b3e5d7
Martin Szyska <martin.szyska37@gmail.com>
2020-11-01 08:46:54 +0100

    identifying bad snps

--- codeCNV/plot.py
+++ codeCNV/plot.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import re
 from matplotlib.patches import Rectangle
 from matplotlib.collections import PatchCollection
 import matplotlib as mpl
@@ -112,7 +113,7 @@ def get_tick_pos(tick_dist, chrom_df):
     return [pos for _, row in chrom_df.iterrows() for pos in range(row['min'] + tick_dist, row['max'], tick_dist)]
 
 
-def str_pos(pos, df, precision=1):
+def str_pos(pos, df, precision=1, print_suff=True):
     '''
     returns string representation of base position
     on genomic coords
@@ -129,12 +130,24 @@ def str_pos(pos, df, precision=1):
         suff = 'Mb'
     elif power10 == 3:
         suff = 'kb'
-    base = round(base, precision)
+    base = re.sub(r'\.0$', "", str(round(base, precision)))
     if power10 == 0:
         suff = 'b'
         base = int(base)
-    return f"{base}{suff}"
+    # only print the suff if print_suff
+    return f"{base}{suff}" if print_suff else base
 
+
+def get_precision(pos_list):
+    '''
+    get the major tick precision from the range of chrom values
+    '''
+    # get the range of positions
+    prange = (max(pos_list) - min(pos_list))
+    # get the closest 10base
+    power10 = int(np.round(np.log10(prange)))
+    precision = max(7 - power10, 0)
+    return precision
 
 def set_ticks(ax, df, chrom_df, ticks=20, label_size=12):
     '''
@@ -157,7 +170,9 @@ def set_ticks(ax, df, chrom_df, ticks=20, label_size=12):
     ax.xaxis.set_major_locator(plt.FixedLocator(major_pos))
     # only print the genomic coords below a certain base total
     if stretch < 2e7:
-        major_labels = [str_pos(pos, df) for pos in major_pos]
+        precision = get_precision(major_pos)
+        print_suff = precision < 3
+        major_labels = [str_pos(pos, df, precision=precision, print_suff=print_suff) for pos in major_pos]
         ax.xaxis.set_major_formatter(plt.FixedFormatter(major_labels))
     else:
         ax.xaxis.set_major_formatter(plt.NullFormatter())
@@ -176,9 +191,9 @@ def extract_pos(region):
 
     def convert(pos):
         if pos.endswith('Mb'):
-            pos = int(pos.replace('Mb', '')) * 1e6
+            pos = int(float(pos.replace('Mb', '')) * 1e6)
         elif pos.endswith('kb'):
-            pos = int(pos.replace('kb', '')) * 1000
+            pos = int(float(pos.replace('kb', '')) * 1000)
         else:
             pos = int(pos)
         return pos
@@ -262,13 +277,13 @@ def plot_genomic(df, plots, chroms='all', color_chroms=True, colormap='coolwarm_
     return fig, ax, df, chrom_df
 
 
-def plot_snp(df, snp_plots=[], cov_plots=[], chroms='all', cov_offset=.25, cov_height=0.5, color_chroms=True, colormap='coolwarm_r', region='', label_size=12, figsize=(20, 4), ylim=(-1, 1)):
-
+def plot_snp(df, snp_plots=[], cov_plots=[], chroms='all', cov_offset=.25, cov_height=0.5, color_chroms=True, colormap='coolwarm_r', region='', label_size=12, figsize=(20,4), ylim=(-1,1)):
+    
     MAXLOG2RATIO = 2.5
     #### DATA MANGELING ##########
     # get cols for rearranging
     org_cols = list(df.columns)
-
+    
     # sort the df
     df = sort_df(df)
     # reduce the df to the selected chromosomes
@@ -278,16 +293,16 @@ def plot_snp(df, snp_plots=[], cov_plots=[], chroms='all', cov_offset=.25, cov_h
     elif chroms != 'all':
         df = df.query('Chr in @chroms')
 
-    # get the chrom_df for collapsing the
+    # get the chrom_df for collapsing the 
     chrom_df = get_chrom_df(df)
-
+    
     df = df.merge(chrom_df.loc[:, 'dif'], on='Chr')
     df['PlotPos'] = df['FullExonPos'] - df['dif']
-
+    
     # rearrange the df as return value
     new_cols = org_cols[:4] + ['PlotPos'] + org_cols[4:]
     df = df.loc[:, new_cols]
-
+    
     #########################
     ######## PLOTTING #######
     # plot the figure
@@ -295,54 +310,65 @@ def plot_snp(df, snp_plots=[], cov_plots=[], chroms='all', cov_offset=.25, cov_h
 
     # set the x-axis limits
     _ = ax.set_xlim(0, df['PlotPos'].max())
-
-    # PLOT COV Data
-
+    
+    
+    ######## PLOT COV Data
+    
     if len(cov_plots):
         scale_factor = cov_height / (MAXLOG2RATIO + 1)
         offset = 1 + scale_factor + cov_offset
 
         ylim = (ylim[0], ylim[1] + cov_offset + cov_height)
-
+        
         for plot in cov_plots:
             # normalize the coverage data:
             # 2.5 is the approx max log2ratio (LOH to 8N)
 
             df[plot['data']] = df[plot['data']] * scale_factor + offset
             if plot['plot_type'] == 'line':
-                plot = ax.plot(df['PlotPos'], df[plot['data']],
-                               **plot['plot_args'])
+                plot = ax.plot(df['PlotPos'],df[plot['data']], **plot['plot_args'])
             elif plot['plot_type'] == 'scatter':
-                plot = ax.scatter(
-                    df['PlotPos'], df[plot['data']], **plot['plot_args'])
-
+                # highjack plot_args
+                pa = plot['plot_args']
+                if "c" in pa:
+                    pa['c'] = df[pa['c']]
+                if "s" in pa:
+                    if isinstance(pa['s'], str):
+                        pa['s'] = df[pa['s']] * 20 + 1
+                plot = ax.scatter(df['PlotPos'],df[plot['data']], **pa)
+    
     ######## plot the SNP graphs #######
     for plot in snp_plots:
         if plot['plot_type'] == 'line':
-            plot = ax.plot(df['PlotPos'], df[plot['data']],
-                           **plot['plot_args'])
+            plot = ax.plot(df['PlotPos'],df[plot['data']], **plot['plot_args'])
         elif plot['plot_type'] == 'scatter':
-            plot = ax.scatter(
-                df['PlotPos'], df[plot['data']], **plot['plot_args'])
-
+            # highjack plot_args with 
+            pa = plot['plot_args']
+            if "c" in pa:
+                    pa['c'] = df[pa['c']]
+            if "s" in pa:
+                if isinstance(pa['s'], str):
+                    pa['s'] = df[pa['s']] * 20 + 1
+            plot = ax.scatter(df['PlotPos'],df[plot['data']], **pa)
+    
     _ = ax.set_ylim(ylim)
     # add the color chroms
-    _ = make_color_chroms(ax, chrom_df, color_chroms,
-                          ylimits=ax.get_ylim(), colormap=colormap)
-
+    _ = make_color_chroms(ax, chrom_df, color_chroms, ylimits=ax.get_ylim(), colormap=colormap)
+    
+    
     ######## LABELS ###################
     # set the axis labels
-    _ = ax.set_xlabel('genomic coords', fontsize=1.25*label_size)
+    _ = ax.set_xlabel('genomic coords', fontsize=1.25*label_size);
     # quick fix for one y-label
-    _ = ax.set_ylabel(' / '.join([plot['title']
-                                  for plot in snp_plots]), fontsize=1.25*label_size)
-
+    _ = ax.set_ylabel(' / '.join([plot['title'] for plot in snp_plots]), fontsize=1.25*label_size)
+    
     ######## CHROM LABELS #############
     add_chrom_labels(ax, chrom_df, ax.get_ylim())
-
+    
     ####### X-AXIS ####################
     # set major ticks and grid for chrom
+    
     ax = set_ticks(ax, df, chrom_df, label_size=label_size)
-
+    
     # return fig and ax for further plotting and return edited dataframe
     return fig, ax, df, chrom_df
