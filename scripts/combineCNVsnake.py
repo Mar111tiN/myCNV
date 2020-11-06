1ab7e88acab0f560602999012d3b98946affe855
Martin Szyska <martinszyska37@gmail.com>
2020-11-06 07:13:35 +0100

    look at indiv traces

--- scripts/combineCNVsnake.py
+++ scripts/combineCNVsnake.py
@@ -1,30 +1,26 @@
+
 import os
 import sys
 import pandas as pd
 
-from script_utils import set_path
+from script_utils import set_path, show_output
+from rollingCNV import rollingCNV
 # get the run_shell function to be passed to running code and set PYTHONPATH
 run_shell = set_path('codeCNV', snakemake)
-
-
-from script_utils import show_output
-from rollingCNV import rollingCNV
 
 
 def main(s):
     '''
     snakemake wrapper for CNV combine and rolling metrices
     '''
-    
+
     c = s.config
     w = s.wildcards
     p = s.params
     i = s.input
     o = s.output
 
-    static_path = c['paths']['mystatic']
     cnv_config = c['CNV']['combine']
-
 
     sample = w.sample
 
@@ -33,18 +29,21 @@ def main(s):
     cnvPON_path = os.path.join(c['paths']['mystatic'], c['ref']['PONcov'])
 
     # run the function
-    show_output(f"Combining CNV data for sample {sample} and computing rolling metrices", time=True)
-    cov_df, snp_df, rCov_df, rSNP_df = rollingCNV(sample, 
-        sample_cnv_path=cnv_path, 
-        PON_cnv_path=cnvPON_path,
-        config=cnv_config)
+    show_output(
+        f"Combining CNV data for sample {sample} and computing rolling metrices", time=True)
+    cov_df, snp_df, rCov_df, rSNP_df = rollingCNV(sample,
+                                                  sample_cnv_path=cnv_path,
+                                                  PON_cnv_path=cnvPON_path,
+                                                  config=cnv_config)
 
-    show_output(f"Rolling metrix computation for sample {sample} finished.", time=True, color='success')
+    show_output(
+        f"Rolling metrix computation for sample {sample} finished.", time=True, color='success')
     cov_df.to_csv(o.cov, sep='\t', index=False)
     snp_df.to_csv(o.snp, sep='\t', index=False)
     rCov_df.to_csv(o.rcov, sep='\t', index=False)
     rSNP_df.to_csv(o.rsnp, sep='\t', index=False)
-    show_output(f"Finished writing output for sample {sample}.", time=True, color='success')
+    show_output(
+        f"Finished writing output for sample {sample}.", time=True, color='success')
 
 
 if __name__ == "__main__":
